0c3f6bd91de214113cef01eb037954bd6c5d2fbc
h3xside <hexside.business@gmail.com>
2025-07-31 14:44:51 +0000

    added marathon stats

---
+++ src/app/catalogue/marathon.ts
@@ -0,0 +1,96 @@
+import { AmmoInfo, AmmoStats } from "../arsenal/ammo"
+import { Weapon, ActionType } from "../arsenal/weapon"
+
+const BASE_DAMAGE = 1113
+const OPTIMAL_RANGE = 20
+const DROP_RANGE = 140
+const SPREAD = 15
+const VERTICAL_RECOIL = 7
+const MUZZLE_VELOCITY = 430
+const AMMO_RESERVE = 24
+
+const AMMO = [
+    new AmmoStats({
+        info: AmmoInfo.COMPACT,
+        scarce: false,
+
+        baseDamage: BASE_DAMAGE,
+        optimalRange: OPTIMAL_RANGE,
+        dropRange: DROP_RANGE,
+        spread: SPREAD,
+        verticalRecoil: VERTICAL_RECOIL,
+        muzzleVelocity: MUZZLE_VELOCITY,
+        ammoReserve: AMMO_RESERVE
+    }),
+    new AmmoStats({
+        info: AmmoInfo.COMPACT_FMJ,
+        scarce: false,
+
+        baseDamage: BASE_DAMAGE,
+        optimalRange: OPTIMAL_RANGE + 10,
+        dropRange: DROP_RANGE - 15,
+        spread: SPREAD,
+        verticalRecoil: VERTICAL_RECOIL + 2,
+        muzzleVelocity: MUZZLE_VELOCITY - 70,
+        ammoReserve: AMMO_RESERVE
+    }),
+    new AmmoStats({
+        info: AmmoInfo.COMPACT_HIGH_VELOCITY,
+        scarce: false,
+
+        baseDamage: BASE_DAMAGE - 8,
+        optimalRange: OPTIMAL_RANGE,
+        dropRange: DROP_RANGE + 30,
+        spread: SPREAD,
+        verticalRecoil: VERTICAL_RECOIL + 2,
+        muzzleVelocity: MUZZLE_VELOCITY + 125,
+        ammoReserve: AMMO_RESERVE - 8
+    }),
+    new AmmoStats({
+        info: AmmoInfo.COMPACT_POISON,
+        scarce: false,
+
+        baseDamage: BASE_DAMAGE,
+        optimalRange: OPTIMAL_RANGE,
+        dropRange: DROP_RANGE,
+        spread: SPREAD,
+        verticalRecoil: VERTICAL_RECOIL,
+        muzzleVelocity: MUZZLE_VELOCITY,
+        ammoReserve: AMMO_RESERVE
+    }),
+    new AmmoStats({
+        info: AmmoInfo.COMPACT_INCENDIARY,
+        scarce: false,
+
+        baseDamage: BASE_DAMAGE,
+        optimalRange: OPTIMAL_RANGE,
+        dropRange: DROP_RANGE,
+        spread: SPREAD,
+        verticalRecoil: VERTICAL_RECOIL,
+        muzzleVelocity: MUZZLE_VELOCITY,
+        ammoReserve: AMMO_RESERVE
+    }),
+]
+
+export const MARATHON: Weapon = new Weapon({
+    name: "Marathon",
+    cost: 68,
+    size: 3,
+    action: ActionType.PUMP_ACTION,
+
+    availableAmmo: AMMO,
+
+    sway: 77,
+    rateOfFire: 31,
+    cycleTime: 1,
+    reloadTime: 19.2,
+    magazine: 15,
+    hasExtraBullet: true,
+
+    meleeDamage: 27,
+    staminaConsumption: 12,
+    heavyMeleeDamage: 54,
+    heavyStaminaConsumption: 25,
+
+    image: "https://huntshowdown.wiki.gg/images/thumb/b/b5/Weapon_Marathon.png/256px-Weapon_Marathon.png"
+})
