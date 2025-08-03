ba52c16b6fb2e3d21d5b7ec158339e3feaa61354
h3xside <hexside.business@gmail.com>
2025-08-03 10:18:59 +0000

    added stats for krag

---
+++ src/app/catalogue/krag.ts
@@ -0,0 +1,88 @@
+import { AmmoStats, AmmoInfo } from "../arsenal/ammo";
+import { ActionType, Weapon } from "../arsenal/weapon";
+
+const BASE_DAMAGE = 126
+const OPTIMAL_RANGE = 40
+const DROP_RANGE = 120
+const SPREAD = 30
+const VERTICAL_RECOIL = 3
+const MUZZLE_VELOCITY = 610
+const AMMO_RESERVE = 10
+
+const NAME = "Krag"
+const COST = 450
+const SIZE = 3
+const ACTION = ActionType.BOLT_ACTION
+const SWAY = 77
+const RATE_OF_FIRE = 23
+const CYCLE_TIME = 1.4
+const RELOAD_TIME = 11.1
+const MAGAZINE = 5 
+const HAS_EXTRA_BULLET = true
+const MELEE_DAMAGE = 27
+const STAMINA_CONSUMPTION = 12
+const HEAVY_MELEE_DAMAGE = 54
+const HEAVY_STAMINA_CONSUMPTION = 25
+const IMAGE = "https://huntshowdown.wiki.gg/images/2/26/Weapon_Berthier_1892.png"
+
+const AMMO = [
+    new AmmoStats({
+        info: AmmoInfo.LONG,
+        scarce: false,
+
+        baseDamage: BASE_DAMAGE,
+        optimalRange: OPTIMAL_RANGE,
+        dropRange: DROP_RANGE,
+        spread: SPREAD,
+        verticalRecoil: VERTICAL_RECOIL,
+        muzzleVelocity: MUZZLE_VELOCITY,
+        ammoReserve: AMMO_RESERVE
+    }),
+    new AmmoStats({
+        info: AmmoInfo.LONG_INCENDIARY,
+        scarce: false,
+
+        baseDamage: BASE_DAMAGE,
+        optimalRange: OPTIMAL_RANGE,
+        dropRange: DROP_RANGE,
+        spread: SPREAD,
+        verticalRecoil: VERTICAL_RECOIL,
+        muzzleVelocity: MUZZLE_VELOCITY,
+        ammoReserve: AMMO_RESERVE
+    }),
+    new AmmoStats({
+        info: AmmoInfo.LONG_SUBSONIC,
+        scarce: false,
+
+        baseDamage: BASE_DAMAGE -20,
+        optimalRange: OPTIMAL_RANGE,
+        dropRange: DROP_RANGE -30,
+        spread: SPREAD,
+        verticalRecoil: VERTICAL_RECOIL,
+        muzzleVelocity: MUZZLE_VELOCITY -274,
+        ammoReserve: AMMO_RESERVE +2
+    })
+]
+
+export const KRAG: Weapon = new Weapon({
+    name: NAME,
+    cost: COST,
+    size: SIZE,
+    action: ACTION,
+
+    availableAmmo: AMMO,
+
+    sway: SWAY,
+    rateOfFire: RATE_OF_FIRE,
+    cycleTime: CYCLE_TIME,
+    reloadTime: RELOAD_TIME,
+    magazine: MAGAZINE,
+    hasExtraBullet: HAS_EXTRA_BULLET,
+
+    meleeDamage: MELEE_DAMAGE,
+    staminaConsumption: STAMINA_CONSUMPTION,
+    heavyMeleeDamage: HEAVY_MELEE_DAMAGE,
+    heavyStaminaConsumption: HEAVY_STAMINA_CONSUMPTION,
+
+    image: IMAGE
+})
