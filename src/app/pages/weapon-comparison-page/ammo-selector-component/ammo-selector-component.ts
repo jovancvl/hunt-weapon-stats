f0690865c7479961cccbada65b28205875a4a7d1
jovancvl <nikolicjovan@gmail.com>
2026-06-17 13:14:21 +0200

    create ammo selector component

---
+++ src/app/pages/weapon-comparison-page/ammo-selector-component/ammo-selector-component.ts
@@ -0,0 +1,33 @@
+import { Component, computed, input, model } from '@angular/core';
+import { Weapon } from '../../../model/weapon';
+import { AmmoStats } from '../../../model/ammo-stats';
+
+@Component({
+  selector: 'hunt-ammo-selector',
+  imports: [],
+  templateUrl: './ammo-selector-component.html',
+  styleUrl: './ammo-selector-component.scss',
+})
+export class AmmoSelectorComponent {
+  weapon = model.required<Weapon>();
+  canChangeActiveAmmo = input<boolean>(false)
+
+  sizeSrc = computed(() => {
+    if (this.weapon().size > 0 && this.weapon().size < 6) {
+      return `ammo-icons/ammo_filter-${this.weapon().size}-slot.svg`;
+    } else {
+      return `ammo-icons/ammo_filter-5-slot.svg`;
+    }
+  });
+
+  activateAmmo(ammo: AmmoStats) {
+    if (!this.canChangeActiveAmmo()) {
+      return;
+    }
+
+    this.weapon.update(w => {
+      w.activeAmmo = ammo
+      return w
+    })
+  }
+}
--- src/app/pages/weapon-comparison-page/weapon-comparison-page.ts
+++ src/app/pages/weapon-comparison-page/weapon-comparison-page.ts
@@ -1,32 +1,23 @@
 import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
-import { SelectWeaponComponent } from "../../components/select-weapon-component/select-weapon-component";
-import { HunterBodyComponent } from "../../components/hunter-body-component/hunter-body-component";
 import { FRONTIER_73C } from '../../catalogue/frontier-73c'
 import { AmmoStats } from '../../model/ammo-stats'
 import { Weapon } from '../../model/weapon'
-import { StatComparisonTableComponent } from "../../components/stat-comparison-table-component/stat-comparison-table-component";
 import { BERTHIER_1892 } from '../../catalogue/berthier-1892'
 import { Dialog } from '@angular/cdk/dialog'
 import { SelectWeaponDialogComponent } from './select-weapon-dialog-component/select-weapon-dialog-component'
-
-enum LeftOrRight {
-  LEFT, 
-  RIGHT
-}
+import { AmmoSelectorComponent } from "./ammo-selector-component/ammo-selector-component";
 
 @Component({
   selector: 'hunt-weapon-comparison-page',
-  imports: [HunterBodyComponent, StatComparisonTableComponent],
+  imports: [AmmoSelectorComponent],
   templateUrl: './weapon-comparison-page.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './weapon-comparison-page.scss',
 })
 export class WeaponComparisonPage {
   dialog = inject(Dialog)
-  left: Weapon = Object.assign({ ...FRONTIER_73C })
-  right: Weapon = Object.assign({ ...BERTHIER_1892 })
-
-  lor = LeftOrRight
+  leftWeapon: Weapon = Object.assign({ ...FRONTIER_73C })
+  rightWeapon: Weapon = Object.assign({ ...BERTHIER_1892 })
 
   activateAmmo(leftOrRight: Weapon, ammo: AmmoStats) {
     leftOrRight.activeAmmo = ammo
@@ -39,9 +30,9 @@ export class WeaponComparisonPage {
         return
       }
       if (side === "left") {
-        this.left = result
+        this.leftWeapon = result
       } else {
-        this.right = result
+        this.rightWeapon = result
       }
     })
   }
